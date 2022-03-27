2b43c457480e837c59f16920a1bed0c55fd4bf6d
Pessoa067 <gui.acelestino@gmail.com>
2022-03-27 18:32:30 -0300

    Criei a cena tutorial e organizei algumas pastas, e criei um prefab
    Cria a cena tutorial , onde eu irei criar e testar as mecanicas que serão apresentadas no tutorial, além disso  criei algumas pastas para e movi certos assets para deixar o projeto mais organizado, por fim crie um prefab dos jogadores e da parede quebrada

---
+++ Assets/Scripts/Parede_quebrada.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parede_quebrada : MonoBehaviour
+{
+    public GameObject parede;
+    public GameObject paredeQuebrada;
+    
+    [Space]
+    [Space]
+
+    public float tempoParaQuebrar;
+    
+    [Space]
+    [Space]
+
+    public Transform centroExplosao;
+    public float forcaExplosao;
+    public float explosaoRadio;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player_1" && PlayerControl.interact == true)
+        {
+            print("é ele");
+            StartCoroutine(QuebrarParede());
+        }
+    }
+
+
+    IEnumerator QuebrarParede()
+    {
+        yield return new WaitForSeconds(tempoParaQuebrar);
+        parede.SetActive(false);
+        paredeQuebrada.SetActive(true);
+
+        for (int i = 0; i < paredeQuebrada.transform.childCount; i++)
+        {
+            GameObject child = paredeQuebrada.transform.GetChild(i).gameObject;
+
+            child.GetComponent<Rigidbody>().AddExplosionForce(forcaExplosao, centroExplosao.position, explosaoRadio);
+
+        }
+
+    }
+}
