e3ba444732a3aba9286260a380a3a6a2fbe1b663
Logon Aluno <logonrmlocal@fiap.com.br>
2022-05-19 10:15:37 -0300

    Organização da pasta de cenas, criação da cena menu , adição do Text Mesh pro

---
+++ Assets/Scripts/Menus/Botoes_Script.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Botoes_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Menu_BotaoIniciar()
+    {
+        
+    }
+
+    public void Menu_BotaoOpcoes()
+    {
+        print("a");
+    }
+
+    public void Menu_BotaoSair()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Menus/ControladorTelas.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorTelas : MonoBehaviour
+{
+    public static int id;
+
+    public GameObject[] menus;
+
+    int contador;
+    public int t;
+    public bool teste;
+
+    /*
+    id 0 = tela do menu
+    id 1 = tela de opçoes
+    id 2 = tela seleção de modo(um jogador ou dois jogadores)
+    id 3 = tela de configuração de dispositivo(apenas para modo 2 jogadores)      
+     */
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (teste)
+        {
+            id = t;
+        }
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (i == id)
+            {
+                menus[i].SetActive(true);
+            }
+            else
+            {
+                menus[i].SetActive(false);
+            }
+        }
+    }
+}
