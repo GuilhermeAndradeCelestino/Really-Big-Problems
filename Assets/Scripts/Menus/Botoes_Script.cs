4e1a9e42104a785d524a2230015f631168552e80
Guilherme A. Celestino <56744028+GuilhermeAndradeCelestino@users.noreply.github.com>
2022-09-03 20:57:54 -0300

    Correções na cena 3

--- Assets/Scripts/Menus/Botoes_Script.cs
+++ Assets/Scripts/Menus/Botoes_Script.cs
@@ -79,7 +79,7 @@ public class Botoes_Script : MonoBehaviour
     {
         ControladorTelas.id = 0;
         print(SceneManager.GetActiveScene().name);
-        if(SceneManager.GetActiveScene().name == "cena 1")
+        if(SceneManager.GetActiveScene().name == "Cena 1" || SceneManager.GetActiveScene().name == "Cena 2" || SceneManager.GetActiveScene().name == "Cena 3")
         {
             SceneManager.LoadScene("MenuInicialScene");
         }
