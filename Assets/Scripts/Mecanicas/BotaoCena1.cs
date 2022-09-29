6d040b357b0119b055e9fe322d043e1b06884a02
Guilherme A. Celestino <56744028+GuilhermeAndradeCelestino@users.noreply.github.com>
2022-09-29 06:08:28 -0300

    algumas mudanças

--- Assets/Scripts/Mecanicas/BotaoCena1.cs
+++ Assets/Scripts/Mecanicas/BotaoCena1.cs
@@ -9,6 +9,10 @@ public class BotaoCena1 : MonoBehaviour
     public GameObject botaoVermelho;
     public GameObject botaoVerde;
 
+    public Animator fade;
+    public GameObject cameraCutscene;
+    public GameObject cameraJogador;
+
     public static bool apertouBotao = false;
     bool limitador = true;
 
@@ -24,17 +28,53 @@ public class BotaoCena1 : MonoBehaviour
     {
         if(limitador && apertouBotao) 
         {
-            doorAnimator.SetBool("OpenDoor", true);
-            doorAnimator2.SetBool("Abrir", true);
-            botaoVerde.SetActive(false);
-            botaoVermelho.SetActive(true);
+            StartCoroutine(cutscene());
 
             apertouBotao = false;
             limitador = false;
         }
     }
 
-    
+    IEnumerator cutscene()
+    {
+        Player_1_Script.travaPlayer = true;
+        Player_2_Script.travaPlayer = true;
+        fade.SetBool("FadeIn", true);
+        yield return new WaitForSeconds(0.5f);
+        cameraJogador.SetActive(false);
+        cameraCutscene.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        fade.SetBool("FadeIn", false);
+        fade.SetBool("FadeOut", true);
+        yield return new WaitForSeconds(1f);
+
+        doorAnimator.SetBool("OpenDoor", true);
+        doorAnimator2.SetBool("Abrir", true);
+        botaoVerde.SetActive(false);
+        botaoVermelho.SetActive(true);
+        fade.SetBool("FadeOut", false);
+        fade.SetBool("voltaPadrao", true);
+        yield return new WaitForSeconds(1.5f);
+
+
+        fade.SetBool("FadeIn", true);
+        fade.SetBool("voltaPadrao", false);
+        yield return new WaitForSeconds(0.5f);
+        cameraJogador.SetActive(true);
+        cameraCutscene.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        fade.SetBool("FadeIn", false);
+        fade.SetBool("FadeOut", true);
+        yield return new WaitForSeconds(1f);
+
+        fade.SetBool("FadeOut", false);
+        fade.SetBool("voltaPadrao", true);
+        fade.SetBool("voltaPadrao", false);
+
+        Player_1_Script.travaPlayer = false;
+        Player_2_Script.travaPlayer = false;
+
+    }
 
 
 }
